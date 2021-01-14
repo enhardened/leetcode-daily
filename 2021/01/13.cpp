2539c7215a723ad2b01d60e4db4cc2e8e9be905c
Rafael de Souza <rafael@enhardened.com>
2021-01-14 02:10:39 -0300

    Solution is a little sorter, but feels less readable

--- 2021/01/13.cpp
+++ 2021/01/13.cpp
@@ -6,16 +6,15 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         int l = 0;
-        int r = people.size() - 1;
+        int r = people.size();
         int result = 0;
         
         sort(people.begin(), people.end());
         
-        while (l <= r) {
+        while (l <= --r) {
             if (people[r] + people[l] <= limit)
                 ++l;
             
-            --r;
             ++result;
         }
         

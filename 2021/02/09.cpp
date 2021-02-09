04f349149acfa8a5db6b5f2e25f909498dff6e66
Rafael de Souza <rafael@enhardened.com>
2021-02-09 10:35:31 -0300

    Made the code shorter but I'm not sure if it was an improvement...

--- 2021/02/09.cpp
+++ 2021/02/09.cpp
@@ -1,7 +1,3 @@
-/*
-Problem: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/585/week-2-february-8th-february-14th/3634/
-Title: Convert BST to Greater Tree
-*/
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,21 +10,17 @@ Title: Convert BST to Greater Tree
  * };
  */
 class Solution {
-public:
-    void traverse(TreeNode* node, int *sum) {
-        if (!node) return;
-        
-        traverse(node->right, sum);
-        
-        (*sum) += node->val;
-        node->val = *sum;
-        
-        traverse(node->left, sum);
-    }
-    
+private:
+    int sum;
+public:    
     TreeNode* convertBST(TreeNode* root) {
-        int sum = 0;
-        traverse(root, &sum);
+        if (root != nullptr) {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+        
         return root;
     }
 };

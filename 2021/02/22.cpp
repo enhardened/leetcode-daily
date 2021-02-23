42e95615559752dca67166cea5ad1a33dfc0e6e4
Rafael de Souza <rafael@enhardened.com>
2021-02-23 02:40:14 -0300

    Improve subsequence logic and refactored it into another function

--- 2021/02/22.cpp
+++ 2021/02/22.cpp
@@ -4,31 +4,32 @@ Title: Longest Word in Dictionary through Deleting
 */
 class Solution {
 public:
+    bool isSubsequence(string &s, string &word) {
+        int n = s.length(),
+            m = word.length(),
+            i = 0,
+            j = 0;
+        
+        while (i < n && j < m) {
+            if (s[i] == word[j]) 
+                ++j;
+            
+            ++i;
+        }
+
+        return j == m;
+    }
+    
     string findLongestWord(string s, vector<string>& d) {
         string longest = "";
-        bool isSubsequence;
         
         for (int i = 0; i < d.size(); ++i) {
             // Ignore words smaller than the current longest
             if (longest.length() > d[i].length())
                 continue;
             
-            isSubsequence = false;
-            string::iterator it = d[i].begin();
-                        
-            for (char c : s) {
-                if (*it == c) {
-                    ++it;   
-                    
-                    if (it == d[i].end()) {
-                        isSubsequence = true;
-                        break;
-                    }
-                }
-            }
-            
             // Ignore words that are not a subsequence of s
-            if (!isSubsequence)
+            if (!isSubsequence(s, d[i]))
                 continue;
             
             if (longest.length() < d[i].length() || longest > d[i])

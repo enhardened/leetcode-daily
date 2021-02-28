45479c2ac65f904cb4d4e07105f91188ea5aac37
Rafael de Souza <rafael@enhardened.com>
2021-02-27 23:59:45 -0300

    Second try - Also rejected
    Looks like the limit for positive 32 bit is different from abs(MIN_INT)
    maybe that's something about 0, like 0 is "positive" and there is no -0 (negative zero)
    so, negatives start counting from -1?

--- 2021/02/27.cpp
+++ 2021/02/27.cpp
@@ -1,21 +1,39 @@
 // Problem: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3654/
 class Solution {
-public:
-    int divide(int dividend, int divisor) {
-        int result = 0;
-        bool n1 = dividend < 0,
-             n2 = divisor < 0;
+public:    
+    unsigned int divideUnsigned(unsigned int dividend, unsigned int divisor) {
+        unsigned int result = 0;
         
-        dividend = abs(dividend);
-        divisor = abs(divisor);
+        if (divisor == 1)
+            return dividend;
         
         while (dividend >= divisor) {
             dividend -= divisor;
             ++result;
         }
         
+        return result;
+    }
+    
+    int cap (unsigned int num) {
+        return (int) min((unsigned int) 2147483647, num);
+    }
+    
+    int divide(int dividend, int divisor) {
+        // cout << pow(2, 31) - 1 << endl;
+        
+        bool n1 = dividend < 0,
+             n2 = divisor < 0;
+        
+        int result = cap(divideUnsigned(
+            (unsigned int) abs(dividend), 
+            (unsigned int) abs(divisor)
+        ));
+        
         return (n1 == n2)
             ? result
             : -result;
+        
+        return 0;
     }
 };

916ac7ae3ec90f118c093e2cef5b7a71df7d9184
Rafael de Souza <rafael@enhardened.com>
2021-02-28 00:08:21 -0300

    Accepted solution

--- 2021/02/27.cpp
+++ 2021/02/27.cpp
@@ -1,4 +1,4 @@
-// Problem: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3654/
+// Problem: https://leetcode.com/problems/divide-two-integers
 class Solution {
 public:    
     unsigned int divideUnsigned(unsigned int dividend, unsigned int divisor) {
@@ -15,25 +15,15 @@ public:
         return result;
     }
     
-    int cap (unsigned int num) {
-        return (int) min((unsigned int) 2147483647, num);
-    }
-    
     int divide(int dividend, int divisor) {
-        // cout << pow(2, 31) - 1 << endl;
         
-        bool n1 = dividend < 0,
-             n2 = divisor < 0;
-        
-        int result = cap(divideUnsigned(
+        unsigned int result = divideUnsigned(
             (unsigned int) abs(dividend), 
             (unsigned int) abs(divisor)
-        ));
+        );
         
-        return (n1 == n2)
-            ? result
-            : -result;
-        
-        return 0;
+        return ((dividend < 0) == (divisor < 0))
+            ? (int) min((unsigned int) 2147483647, result)
+            : (int) -min((unsigned int) 2147483648, result);
     }
 };

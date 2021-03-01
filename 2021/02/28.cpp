9a850d9fe1c4a1ca40c179a0b019093051c5ed9c
Rafael de Souza <rafael@enhardened.com>
2021-03-01 00:17:59 -0300

    Accepted submission

--- 2021/02/28.cpp
+++ 2021/02/28.cpp
@@ -1,49 +1,32 @@
 // Problem: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3655/
 class FreqStack {
+private:
+    int maxFreq = 0;
+    unordered_map<int, int> freq;
+    unordered_map<int, stack<int, list<int>>> groups;
+
 public:
-    unordered_map<int, int> frequences;
-    vector<int> nums;
-    
     FreqStack() {
-        
     }
     
     void push(int x) {
-        ++frequences[x];
-        nums.push_back(x);
+        ++freq[x];
+        groups[freq[x]].push(x);
+        
+        maxFreq = max(maxFreq, freq[x]);
     }
     
     int pop() {
-        int popped;
-        int highest_freq = 0;
-        vector<int> most_freq_nums;
+        int num = groups[maxFreq].top();
         
-        for (auto frequency : frequences)
-            highest_freq = max(highest_freq, frequency.second);
+        groups[maxFreq].pop();
         
-        for (auto frequency : frequences)
-            if (highest_freq == frequency.second)
-                most_freq_nums.push_back(frequency.first);
+        if (groups[maxFreq].empty())
+            --maxFreq;
         
-        bool found = false;
+        --freq[num];
         
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            for (int j = 0; j < most_freq_nums.size(); ++j) {
-               if (most_freq_nums[j] == nums[i]) {
-                   popped = nums[i];
-                   nums.erase(nums.begin() + i);
-                   found = true;
-                   break;
-               } 
-            }
-            
-            if (found)
-                break;
-        }
-        
-        --frequences[popped];
-        
-        return popped;
+        return num;
     }
     
 };

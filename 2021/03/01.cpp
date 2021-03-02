3d726e5d1392e0b0acbd89516da7e43c912f56c2
Rafael de Souza <rafael@enhardened.com>
2021-03-01 23:16:33 -0300

    Update set creation to use unordered_set's constructor

--- 2021/03/01.cpp
+++ 2021/03/01.cpp
@@ -4,17 +4,9 @@ public:
     int distributeCandies(vector<int>& candyType) {
         int n = candyType.size();
         int limit = n / 2;
-        int type_count = 0;
         
-        unordered_set<int> seen_types;
+        unordered_set<int> seen_types(candyType.begin(), candyType.end());
         
-        for (int i = 0; i < n; ++i) {
-            if (seen_types.count(candyType[i]) == 0) {
-                ++type_count;
-                seen_types.insert(candyType[i]);
-            }
-        }
-        
-        return min(type_count, limit);
+        return min(limit, (int) seen_types.size());
     }
 };

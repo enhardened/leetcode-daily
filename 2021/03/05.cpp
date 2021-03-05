23ba6c7ec42e930a601df8913050531cd758c3f7
Rafael de Souza <rafael@enhardened.com>
2021-03-05 17:14:42 -0300

    Replace recursive DFS solution with iteractive BFS

--- 2021/03/05.cpp
+++ 2021/03/05.cpp
@@ -1,46 +1,49 @@
 // Problem: https://leetcode.com/problems/average-of-levels-in-binary-tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<pair<long int, int>> data;
-    
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> result;
-        // To find the average value of each level
-        // I need to know the sum of all nodes on each level
-        // and the number of nodes on each level
+        if (!root)
+            return {};
         
-        // One possibility is to traverse the tree 
-        // while creating a map of level to [sum, count] 
-        // then iterate over the map dividing sum by count 
-        // and appending the division result to the result array
-        // Time complexity would be O(n), n being the number of nodes
-        // Space complecity would be O(m), m being the lenght of the deepest level of the tree 
+        std::vector<double> result;
         
-        // Is there a more efficient way to calculate the average?
-        // O(n) time seens to be the minium possible since it will need to know every node value
-        // linear space would be the minium possible to store each level's average before returning
-        // But is it possible to reduce the angular coefficient of the space complexity?
-        // - I am unable find a more efficient solution at this time
+        // Trying iterative BFS instead of recursive DFS
+        std::stack<std::pair<TreeNode*, int>> stack;
+        std::vector<std::pair<double, int>> data;
         
-        traverse(root, 0);
+        stack.push({root, 0});
         
-        for (auto &row : data)
-            result.push_back((double) row.first / row.second);
+        while (!stack.empty()) {
+            auto [node, level] = stack.top();
+            stack.pop();
+            
+            if (data.size() == level)
+                data.push_back({node->val, 1});
+            else
+                data[level].first += node->val,
+                data[level].second++;
+
+            if (node->left) 
+                stack.push({node->left, level + 1});
+            
+            if (node->right) 
+                stack.push({node->right, level + 1});
+        }
+        
+        for (auto [sum, count] : data)
+            result.push_back(sum / count);
         
         return result;
-    }
-    
-    void traverse(TreeNode *node, int level) {
-        if (!node)
-            return;
-        
-        if (data.size() == level)
-            data.push_back(make_pair(node->val, 1));
-        else
-            data[level].first += node->val,
-            ++data[level].second;
-        
-        traverse(node->left, level + 1);
-        traverse(node->right, level + 1);
     }
 };

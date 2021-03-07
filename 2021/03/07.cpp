608b04f50fa6d3c5b5838e523695857208a6f097
Rafael de Souza <rafael@enhardened.com>
2021-03-07 11:13:46 -0300

    Reduce the number of initial buckets to create some collisions

--- 2021/03/07.cpp
+++ 2021/03/07.cpp
@@ -1,26 +1,32 @@
 // Problem: https://leetcode.com/explore/item/3663
 class MyHashMap {
 public:
-    vector<int> table = vector<int>(1000000, -1);
+    vector<vector<int>> table;
     
     /** Initialize your data structure here. */
     MyHashMap() {
-        
+        table.resize(1000);
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) {
-        table[key] = value;
+        int hash = key / 1000;
+        if (table[hash].empty())
+            table[hash].resize(1000, -1);
+        table[hash][key % 1000] = value;
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        return table[key];
+        return (table[key / 1000].empty())
+            ? -1
+            : table[key / 1000][key % 1000];
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        table[key] = -1;
+        if (!table[key / 1000].empty())
+            table[key / 1000][key % 1000] = -1;
     }
 };
 
@@ -31,3 +37,4 @@ public:
  * int param_2 = obj->get(key);
  * obj->remove(key);
  */
+ 

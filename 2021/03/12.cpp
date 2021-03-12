4f51cf437ddb3aaf19d33955c4911eb82a05cf32
Rafael de Souza <rafael@enhardened.com>
2021-03-12 17:47:49 -0300

    Improve execution time and memory usage by returning early when s isn't long enought

--- 2021/03/12.cpp
+++ 2021/03/12.cpp
@@ -3,8 +3,12 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) {
         unordered_set<string> codes;
-        int expectedSize = pow(2, k),
-            n = s.length() - k;
+        int expectedSize = 1 << k;
+        
+        if (s.length() < expectedSize + k - 1)
+            return false;
+        
+        int n = s.length() - k;
         
         for (int i = 0; i <= n && codes.size() < expectedSize; ++i)
             codes.insert(s.substr(i, k));

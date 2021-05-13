9d1f1dff4d0b62b7ae28b38cfe07eb86b0bfccde
Rafael de Souza <rafael@enhardened.com>
2021-05-13 00:26:25 -0300

    Simplify the solution for LeetCode's problem 303

--- leetcode/303.cpp
+++ leetcode/303.cpp
@@ -5,20 +5,18 @@ public:
     vector<int> memo;
     
     NumArray(vector<int>& nums) {
-        this->memo.resize(nums.size());
-        this->memo[0] = nums[0];
+        int n = nums.size() + 1;
+        
+        this->memo.resize(n);
+        this->memo[0] = 0;
             
-        for (int i = 1; i < nums.size(); ++i) {
-            this->memo[i] = this->memo[i-1] + nums[i];
+        for (int i = 1, p = 0; i < n; ++i, ++p) {
+            this->memo[i] = this->memo[p] + nums[p];
         }
     }
     
     int sumRange(int left, int right) {
-        if (left == 0) {
-            return memo[right];
-        }
-        
-        return memo[right] - memo[left-1];
+        return memo[right + 1] - memo[left];
     }
 };
 

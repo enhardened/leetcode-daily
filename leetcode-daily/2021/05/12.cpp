ebde2ed74e65eb85651d93b97eae0a84bb636941
Rafael de Souza <rafael@enhardened.com>
2021-05-13 00:53:34 -0300

    Improve solution for 2021/05/12's challenge from LeetCode with DP

--- leetcode-daily/2021/05/12.cpp
+++ leetcode-daily/2021/05/12.cpp
@@ -1,21 +1,30 @@
 // Problem: Range Sum Query 2D - Immutable
 // URL: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3740/
-// Naive solution (Timed out)
 class NumMatrix {
 private:
-    vector<vector<int>> *matrix;
+    
 public:
+    vector<vector<int>> memo;
+    
     NumMatrix(vector<vector<int>>& matrix) {
-        this->matrix = &matrix;
+        int m = matrix.size(),
+            n = matrix[0].size() + 1;
+        
+        memo.resize(m, vector<int>(n));
+        
+        for (int i = 0; i < m; ++i) {
+            memo[i][0] = 0;
+            for (int j = 1; j < n; ++j) {
+                memo[i][j] = memo[i][j-1] + matrix[i][j-1];
+            }
+        }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
         int sum = 0;
         
         for (int i = row1; i <= row2; ++i) {
-            for (int j = col1; j <= col2; ++j) {
-                sum += (*matrix)[i][j];
-            }
+            sum += memo[i][col2 + 1] - memo[i][col1];
         }
         
         return sum;

0524d0802e740ee49014870e5a04c44b527a0ee4
Rafael de Souza <rafael@enhardened.com>
2021-05-17 00:15:38 -0300

    Reduce constant factors

--- leetcode-daily/2021/05/16.cpp
+++ leetcode-daily/2021/05/16.cpp
@@ -13,41 +13,35 @@
  */
 class Solution {
 public:
-    int result = 0;
-    
     int minCameraCover(TreeNode* root) {
-        dfs(root, nullptr);
-        return result;
+        int result = 0;
+        
+        return dfs(root, nullptr, result) == -1
+            ? result + 1
+            : result;
     }
 
     // return remaining coverage
     // 1 if node is has a cam
     // 0 if a child has a cam
     // -1 if neither the node or both its children doesn't have a cam
-    int dfs(TreeNode *node, TreeNode *parent) {
+    int dfs(TreeNode *node, TreeNode *parent, int &result) {
         if (!node)
             return 0;
         
-        int lrd = dfs(node->left, node);
-        int rrd = dfs(node->right, node);
+        int l = dfs(node->left, node, result);
+        int r = dfs(node->right, node, result);
         
-        bool isCovered = lrd == 1 || rrd == 1;
-        bool hasUncoveredChild = lrd == -1 || rrd == -1;
-        
-        // ---
-        if (hasUncoveredChild) {
+        // Any child not covered
+        if (l == -1 || r == -1) {
             result++;
             return 1;
-        } else if (isCovered) {
-            return 0;
-        } else {
-            if (parent)
-                return -1;
-            
-            result++;
-        }
-        // ---
+        } 
         
-        return 0;
+        // Covered by any child
+        if (l == 1 || r == 1)
+            return 0;
+        
+        return -1;
     }
 };
